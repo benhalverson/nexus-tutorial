0cbacfa310151c098d38ea6ceb473b8d237501dc
Ben Halverson <benhalverson33@gmail.com>
2021-09-06 01:15:16 -0700

    step 2...

---
+++ api/graphql/Post.ts
@@ -0,0 +1,30 @@
+import { extendType, objectType } from 'nexus';
+
+export const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.int('id');
+    t.string('title');
+    t.string('body');
+    t.boolean('published');
+  },
+});
+
+export const PostQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('drafts', {
+      type: 'Post',
+      resolve() {
+        return [
+          {
+            id: 1,
+            title: 'Hello',
+            body: 'World',
+            published: false,
+          },
+        ];
+      },
+    });
+  },
+});
